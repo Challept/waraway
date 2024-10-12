5cc87f60ea7e261ac03c24ebb7c11f7d55d5e76c
Challept <119951713+Challept@users.noreply.github.com>
2024-10-12 16:54:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -924,6 +924,47 @@ const countryTranslations = {
   "finland": "Finland"
   // Lägg till fler översättningar om det behövs
 };
+function findClosestCountry(input, countryList) {
+    input = input.toLowerCase();
+    let closestCountry = '';
+    let shortestDistance = Infinity;
+
+    countryList.forEach(country => {
+        const distance = levenshteinDistance(input, country.toLowerCase());
+        if (distance < shortestDistance) {
+            shortestDistance = distance;
+            closestCountry = country;
+        }
+    });
+
+    return closestCountry;
+}
+
+function levenshteinDistance(a, b) {
+    const matrix = [];
+
+    if (a.length === 0) return b.length;
+    if (b.length === 0) return a.length;
+
+    for (let i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+    for (let j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) === a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
+}
 
 function translateCountry(input) {
   const lowerCaseInput = input.toLowerCase();
@@ -936,19 +977,19 @@ function translateCountry(input) {
 }
 
 function formatNumber(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Tusentalsavgränsare
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function calculateMilitaryScore(military, population) {
-  const militaryStrengthWeight = 5;  // Mycket viktigt
-  const warplanesWeight = 4;         // Mycket viktigt
-  const tanksWeight = 3;             // Viktigt
-  const populationWeight = 2;        // Viktigt
+  const militaryStrengthWeight = 5;
+  const warplanesWeight = 4;
+  const tanksWeight = 3;
+  const populationWeight = 2;
 
-  return (military.military_strength * militaryStrengthWeight) + 
-         (military.warplanes * warplanesWeight) + 
-         (military.tanks * tanksWeight) + 
-         (population * populationWeight); 
+  return (military.military_strength * militaryStrengthWeight) +
+         (military.warplanes * warplanesWeight) +
+         (military.tanks * tanksWeight) +
+         (population * populationWeight);
 }
 
 function generateExplanation(c1Military, c2Military, c1Name, c2Name) {
